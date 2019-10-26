cddf967f141aa47242ef317d9d50ce6ecb5c54fc
Hamza <hamza40nazir@gmail.com>
2019-10-26 21:20:37 +0500

    Login page made

---
+++ app/src/main/java/com/example/familytracker/loginActivity.java
@@ -0,0 +1,48 @@
+package com.example.familytracker;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class loginActivity extends AppCompatActivity {
+
+
+    FirebaseAuth auth;
+    EditText email1, pass1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        email1 = (EditText)findViewById(R.id.editText);
+        pass1  = (EditText)findViewById(R.id.editText2);
+        auth = FirebaseAuth.getInstance();
+
+    }
+
+    public void login(View view)
+    {
+        auth.signInWithEmailAndPassword(email1.getText().toString(),pass1.getText().toString())
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful())
+                        {
+                            Toast.makeText(getApplicationContext(),"Login Successful",Toast.LENGTH_LONG).show();
+                        }
+                        else
+                            Toast.makeText(getApplicationContext(),"Wrong Details Entered",Toast.LENGTH_LONG).show();
+                    }
+                });
+    }
+
+
+}
